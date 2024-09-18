3531beda78ba8e06905f51cc78e21d6feb66049a
Khaibar Saadat <khybersaadat93@gmail.com>
2024-09-18 21:00:59 +0200

    Clearing Tasks & prevent empty task submission with modal validation

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+import Modal from "./Modal";
 
 export default function NewTask({ onAdd }) {
+  const modal = useRef();
   const [enteredTask, setEnteredTask] = useState("");
 
   function handleChange(event) {
@@ -8,24 +11,37 @@ export default function NewTask({ onAdd }) {
   }
 
   function handleClick() {
+    if (enteredTask.trim() === "") {
+      modal.current.open();
+      return;
+    }
+
     onAdd(enteredTask);
     setEnteredTask("");
   }
 
   return (
-    <div className="flex items-center gap-4">
-      <input
-        type="text"
-        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
-        onChange={handleChange}
-        value={enteredTask}
-      />
-      <button
-        className="text-stone-700 hover:text-stone-950"
-        onClick={handleClick}
-      >
-        Add Task
-      </button>
-    </div>
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Please enter a valid text before adding it to the tasks collection.
+        </p>
+      </Modal>
+      <div className="flex items-center gap-4">
+        <input
+          type="text"
+          className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+          onChange={handleChange}
+          value={enteredTask}
+        />
+        <button
+          className="text-stone-700 hover:text-stone-950"
+          onClick={handleClick}
+        >
+          Add Task
+        </button>
+      </div>
+    </>
   );
 }
